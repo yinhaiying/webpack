20914a4d00fbbb1ed9716989ef2ba3d33c70725c
yinhaiying <yinhaiying@58.com>
2021-02-02 14:44:40 +0800

    asyncSeriesHook异步串行钩子

---
+++ packages/tapable/async/3.asyncSerieslHook.js
@@ -0,0 +1,40 @@
+/* 
+AsyncSeriesHook:异步串行。
+async:异步。
+series：序列。
+asyncSeriesHook:异步地按照顺序依次执行，因此是异步串行钩子。
+*/
+
+let {
+    AsyncSeriesHook
+} = require("tapable");
+
+class Lesson {
+    constructor() {
+        this.hooks = {
+            arch: new AsyncSeriesHook(["name"]),
+        }
+    }
+    tap() {
+        this.hooks.arch.tapAsync("js", (name, callback) => {
+            setTimeout(() => {
+                console.log("js课程");
+                callback();
+            }, 5000)
+        });
+        this.hooks.arch.tapAsync("css", (name, callback) => {
+            setTimeout(() => {
+                console.log("css课程");
+                callback();
+            }, 1000)
+        });
+    }
+    start() {
+        this.hooks.arch.callAsync("haiyinsitan", () => {
+            console.log("异步钩子在所有异步hooks执行完毕之后，必须有回调")
+        });
+    }
+}
+let lesson = new Lesson();
+lesson.tap(); // 注册这两个事件
+lesson.start(); // 启动钩子
--- packages/tapable/async/3.custom-asyncSeriesHook.js
+++ packages/tapable/async/3.custom-asyncSeriesHook.js
@@ -1,4 +1,14 @@
-class MyAsyncParallelHook {
+/* 
+AsyncSeriesHook:异步串行。
+async:异步。
+series：序列。
+asyncSeriesHook:异步地按照顺序依次执行，因此是异步串行钩子。
+*/
+let {
+    AsyncSeriesHook
+} = require("tapable");
+
+class MyAsyncSeriesHook {
     constructor(args) {
         this.tasks = [];
     }
@@ -6,24 +16,25 @@ class MyAsyncParallelHook {
         this.tasks.push(task)
     }
     callAsync(...args) {
-        let finalCallback = args.pop();   //最后一个参数是回调函数。
+        let finalCallback = args.pop();
         let index = 0;
-        const done = () => {
-          index += 1;
-          if (index === this.tasks.length) {
-              finalCallback();
+        let next = () => {
+          if(this.tasks.length === index){
+            finalCallback();
+            return;
           }
+          let task = this.tasks[index++];
+          task(...args,next);
         }
-        this.tasks.forEach((task) => {
-            task(...args,done);
-        });
+        next();
     }
 }
+
 
 class Lesson {
     constructor() {
         this.hooks = {
-            arch: new MyAsyncParallelHook(["name"]),
+            arch: new MyAsyncSeriesHook(["name"]),
         }
     }
     tap() {
@@ -31,7 +42,7 @@ class Lesson {
             setTimeout(() => {
                 console.log("js课程");
                 callback();
-            }, 1000)
+            }, 2000)
         });
         this.hooks.arch.tapAsync("css", (name, callback) => {
             setTimeout(() => {
@@ -42,11 +53,10 @@ class Lesson {
     }
     start() {
         this.hooks.arch.callAsync("haiyinsitan", () => {
-            console.log("end:所有异步hooks执行完毕")
+            console.log("异步钩子在所有异步hooks执行完毕之后，必须有回调")
         });
     }
 }
 let lesson = new Lesson();
 lesson.tap(); // 注册这两个事件
-
 lesson.start(); // 启动钩子
